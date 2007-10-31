0b0e602aeb2a897fb0d2494cb007b15c9bdacd68
plotfi@gmail.com <plotfi@gmail.com@f3a4c8be-883b-0410-9dc1-159affa97516>
2007-10-31 10:17:57 +0000

    I forgot to add these. Sorry. StupidContainer.cs is a hack btw. I need to write some legitimate classes for passing to threads.

---
+++ trunk/picasa-sharp/StupidContainer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace picasa_sharp {
+    class StupidContainer {
+
+        public Mono.Google.Picasa.PicasaAlbum album;
+        public string[] filenames;
+        public System.Windows.Forms.ToolStripProgressBar progress;
+
+    }
+}
